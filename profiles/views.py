d2ff09204d1369bcbf52774b19e2ed0c1b8017be
tsGlow <haruki84@msn.com>
2026-06-11 14:24:23 +0900

    feat: add simple upload

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,14 @@
 from django.shortcuts import render
 from django.views import View
-
+from django.http import HttpResponseRedirect
 # Create your views here.
+
+
+
+def store_file(file):    
+    with open("temp/image.jpg", "wb+") as dest:
+        for chunk in file.chunks():
+            dest.write(chunk)
 
 
 class CreateProfileView(View):
@@ -9,4 +16,6 @@ class CreateProfileView(View):
         return render(request, "profiles/create_profile.html")
 
     def post(self, request):
-        pass
+        print(request.FILES["image"])            
+        store_file(request.FILES["image"])
+        return HttpResponseRedirect("/profiles")
