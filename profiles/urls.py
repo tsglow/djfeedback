da8aebe78dd64d8b081b2217ea97f123d3fd7ef1
tsGlow <haruki84@msn.com>
2026-06-12 12:41:03 +0900

    feat: now serving uploaded files

--- profiles/urls.py
+++ profiles/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.CreateProfileView.as_view(), name="profile")
+    path("", views.CreateProfileView.as_view(), name="upload_profile"),
+    path("list", views.ListProfile.as_view(), name="list_profile")
 ]
+
